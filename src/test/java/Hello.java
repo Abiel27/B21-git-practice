1b622488b1bb324c65914c044f93fed05f7cd4fe
Abiel Menghistu <abielmenghistu@gmail.com>
2021-01-26 14:07:31 -0500

    This is gitHub training. More coomite, if i have repo then, I will see. fall in love. This is Github training. messi!

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -1,7 +1,8 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello Cybertek Selam! Selam! Selam");
+        System.out.println("Hello Cybertek  Selam! Selam! Selam");
         System.out.println("I am haveing fun:-)" );
         System.out.println("Hi there this is GitHut training class!");
+        System.out.println("OH MESSI MY GIHUB!");
     }
 }
